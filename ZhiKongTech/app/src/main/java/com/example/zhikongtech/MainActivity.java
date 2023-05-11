8103ebe1535e3823729be4a9e93d5024e00a5e87
ChenJinli <cheneycjl@gmail.com>
2023-05-11 17:18:43 +0800

    Learn

--- ZhiKongTech/app/src/main/java/com/example/zhikongtech/MainActivity.java
+++ ZhiKongTech/app/src/main/java/com/example/zhikongtech/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.zhikongtech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnTextView = findViewById(R.id.btn_textView);
+        mBtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到TextView界面
+                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivities(new Intent[]{intent});
+            }
+        });
     }
 }
