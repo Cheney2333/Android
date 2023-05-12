757f1c7448b6874baddf67ceef26ee5ccd6a4166
ChenJinli <cheneycjl@gmail.com>
2023-05-12 15:02:31 +0800

    基本实现了能够向服务器单向传输信息。

--- ZhiKongTech/app/src/main/java/com/example/zhikongtech/MainActivity.java
+++ ZhiKongTech/app/src/main/java/com/example/zhikongtech/MainActivity.java
@@ -2,44 +2,74 @@ package com.example.zhikongtech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mBtnTextView;
-    private Button mBtnButton;
-    private Button mBtnEditText;
+    private Button sendButton;
+    private EditText commandEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBtnTextView = findViewById(R.id.btn_textView);
-        mBtnTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //跳转到TextView演示界面
 
-            }
-        });
-        mBtnButton = findViewById(R.id.mBtnButton);
-        mBtnButton.setOnClickListener(new View.OnClickListener() {
+        sendButton = findViewById(R.id.sendButton);
+        commandEditText = findViewById(R.id.commandEditText);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 跳转到Button演示界面
-
+                String command = commandEditText.getText().toString();
+                SendMessageTask sendMessageTask = new SendMessageTask();
+                sendMessageTask.execute(command);
             }
         });
-        mBtnEditText = findViewById(R.id.btn_edittext);
-        mBtnEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 跳转到EditText演示界面
 
-            }
-        });
     }
+
+    private class SendMessageTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                // 服务器的IP和端口号
+                String serverIP = "8.140.26.4";
+                int serverPort = 3389;
+
+                // 创建Socket连接
+                Socket socket = new Socket(serverIP, serverPort);
+
+                // 获取输出流
+                OutputStream outputStream = socket.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+                // 发送指令给服务器
+                String message = params[0];
+                bufferedWriter.write(message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                // 关闭连接
+                bufferedWriter.close();
+                outputStream.close();
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
